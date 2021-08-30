528a791bf0492a4b001241929faf38b20f4d25d6
Harsh Sanklecha <sanklecha.harsh@gmail.com>
2021-08-30 22:57:47 +0530

    we-integrate mobile and particle js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@ $(window).on("load", function () {
   },500)
 });
 
+
+// Custom Cursor
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
+
+
 $(".menuBtn").click(function () {
   this.classList.toggle("active");
   if (this.classList.contains("active")) {
@@ -81,6 +101,64 @@ $(".navMenu .navigation a").click(function () {
     ease: "expo.in",
   });
 });
+
+/* Particle JS */
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 300, density: { enable: true, value_area: 800 } },
+    color: { value: "#fd4d4d" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 4, size_min: 0.3, sync: false },
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#fd4d4d",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 600 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+
 
 /* Hero Text Animation */
 window.onscroll = function () {
